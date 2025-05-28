26b2856ab489f445e0892e75a3aef2911942c858
Oscar <omsz90@live.com.mx>
2025-05-27 18:19:26 -0600

    work in progress (#2)

--- api-constantec/main.py
+++ api-constantec/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import FileResponse
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.staticfiles import StaticFiles
 from Routers import estudiantes, constancias, solicitudes, login
 from sqlalchemy.orm import Session
 from Database.database import SessionLocal
@@ -7,6 +9,12 @@ from CRUD import crud_estudiante
 from Schemas import schemas
 
 app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="web-app/assets"), name="assets")
+
+# @app.get("/app")
+# async def serve_vue(full_path: str):
+#     return FileResponse("web-app/index.html")
 
 def get_db():
     db = SessionLocal()
@@ -17,7 +25,8 @@ def get_db():
 
 @app.get("/")
 def iniciando_sesion():
-    return {"mensaje": "API de constancias activa. Usa /docs para ver la documentacion. :)"}
+    return FileResponse("web-app/index.html")
+    # return {"mensaje": "API de constancias activa. Usa /docs para ver la documentacion. :)"}
 
 # app.include_router(login.router, prefix="/v1/login", tags=["Login"])
 app.include_router(estudiantes.router, prefix="/v1/estudiantes", tags=["Estudiantes"])
---
+++ scripts/build_and_publish_docker_dev_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -f ./dockerfiles/Dockerfile.dev -t jeshuarocha/constantec_dev:1.1.0 -t jeshuarocha/constantec_dev:latest .
