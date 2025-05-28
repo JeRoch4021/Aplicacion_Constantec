d96c2181e686687a009cc640a7769a6c63dfe64a
Oscar Martinez Sanchez <osanchez@crunchtime.com>
2025-05-28 16:43:58 -0600

    wip

--- api-constantec/Models/factories.py
+++ api-constantec/Models/factories.py
@@ -10,24 +10,19 @@ from Autenticacion.seguridad import get_password_hash
 
 session = SessionLocal()
 
-def random_upper_alphanumeric(length=10):
-    chars = string.ascii_uppercase + string.digits  # A-Z and 0-9
-    return ''.join(random.choices(chars, k=length))
-
-# Define the factory
 class EstudiantesFactory(SQLAlchemyModelFactory):
     class Meta:
         model = Estudiantes
         sqlalchemy_session = session
         sqlalchemy_session_persistence = "commit"
 
-    no_control = factory.LazyFunction(random_upper_alphanumeric)
+    no_control = factory.Faker("bothify", text="########")
     nombre = factory.Faker("name")
     apellidos = factory.Faker("last_name")
     fecha_nacimiento = factory.Faker("date_of_birth")
     edad = factory.Faker("random_int", min=18, max=25)
     municipio = factory.Faker("city")
-    correo_institucional = factory.LazyAttribute(lambda obj: f"{obj.nombre.lower().replace(" ", "_")}@leon.tecnm.mx")
+    correo_institucional = factory.LazyAttribute(lambda obj: f"{obj.no_control.lower().replace(" ", "_")}@leon.tecnm.mx")
     fecha_registro = factory.LazyFunction(datetime.date.today)
     contrasena = get_password_hash('test')
     primer_ingreso = False
