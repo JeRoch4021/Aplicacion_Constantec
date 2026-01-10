86ad9058f4ac164b5471855e9e80d51b58547a0c
Oscar <omsz90@live.com.mx>
2026-01-09 18:06:28 -0600

    Revert "Working on serveral tasks, database creation at startup, fast admin i…" (#5)
    This reverts commit 8ef85af0f252bb9c814d10333f5adb62230ebd60.

--- api-constantec/Comun/response.py
+++ api-constantec/Comun/response.py
@@ -1,10 +1,10 @@
-from typing import Any
-
 from pydantic import BaseModel
-
+from typing import Any
 
 class Response(BaseModel):
     success: bool
     messsage: str
     error_code: str | None
     data: Any
+
+
